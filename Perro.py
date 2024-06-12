1542ab11d231243dbab898d73b835fcdef9dde62
Albert0PB <a23pebeal@iesgrancapitan.org>
2024-06-12 08:59:46 +0200

    Documentación 1 - Alberto Pérez Bernabeu

--- Perro.py
+++ Perro.py
@@ -4,8 +4,14 @@ Clase Perro.
 Autor: Jaime Rabasco Ronda.
 """
 class Perro:
+    """
+    Implementación de una clase 'Perro'.
+    """
 
     def ladrar(self):
+        """
+        Método que hace ladrar a la instancia de 'Perro'.
+        """
         self.ladra = 'Guau'
         print(self.ladra);
 
